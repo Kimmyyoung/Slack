90661343be68c1cf7c44af1e36d4f93ea9ba68fe
kimmyyoung <hiyoung.kimmy@gmail.com>
2023-03-09 20:52:58 -0800

    ✨ feat: Add workspace with Gravatar

--- layouts/Workspace/index.tsx
+++ layouts/Workspace/index.tsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 import useSWR from 'swr';
 import fetcher from "@utils/fetcher";
 import {Navigate} from 'react-router-dom';
+import { Header, RightMenu, ProfileImg, WorkspaceWrapper, Workspaces, Channels, Chats, WorkspaceName, MenuScroll } from "@layouts/Workspace/style";
+import gravatar from 'gravatar';
 
 const Workspace : VFC = ()=>{
     const  {data, error, mutate} = useSWR('http://localhost:3095/api/users', fetcher, {
@@ -26,7 +28,26 @@ const Workspace : VFC = ()=>{
 
     return (
         <div>
+            <Header>test</Header>
+            <RightMenu>
+                <span>
+                    <ProfileImg src={gravatar.url(data.nickname, {s:'28px', d: 'retro'})} alt="Profile Image" />
+                </span>
+            </RightMenu>
+
         <button onClick={onLogout}>Logout</button>
+        <WorkspaceWrapper>
+            <Workspaces>
+                TEST
+            </Workspaces>
+            <Channels>
+                <WorkspaceName>Sleact</WorkspaceName>
+                <MenuScroll>
+                    {/* <Menu></Menu> */}
+                </MenuScroll>
+            </Channels>
+            <Chats>CHATS</Chats>
+        </WorkspaceWrapper>
         </div>
     )
 };
